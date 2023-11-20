cd644f9ab74149ca709c4005555921ba1e349d25
AjaxAueleke <ahmed.jamil7410@gmail.com>
2023-11-20 20:51:41 +0000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-   let error_msg = "Should have been able to read the file";
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing args: {err}");
+        process::exit(1);
+    });
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.file_path);
 
-    
-    let contents = fs::read_to_string(config.file_path).expect(error_msg);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+   
+}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("{}",contents);
+    Ok(())
     
 }
 struct Config  {
@@ -20,14 +31,14 @@ struct Config  {
 }
 
 impl Config  {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }   
+        Ok(Config { query, file_path })
     }
 }
 
