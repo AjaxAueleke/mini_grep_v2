c3350f3c86b67a1afb790bcee8427276ba7dddff
AjaxAueleke <ahmed.jamil7410@gmail.com>
2023-11-20 21:10:34 +0000

    remove unused variables, parameters, etc.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use Rust_Practice::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|_| {
         process::exit(1);
     });
 
-    if let Err(e) = Rust_Practice::run(config) {
+    if let Err(_) = Rust_Practice::run(config) {
         process::exit(1);
     }
    
