23300bc231d8dd94fdc557f9719a267d608ea306
AjaxAueleke <ahmed.jamil7410@gmail.com>
2023-11-20 18:51:25 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-    dbg!(args);
+   let error_msg = "Should have been able to read the file";
+    let config = Config::new(&args);
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
+
+    
+    let contents = fs::read_to_string(config.file_path).expect(error_msg);
+    println!("{}",contents);
+    
 }
+struct Config  {
+    query: String,
+    file_path: String,
+}
+
+impl Config  {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }   
+    }
+}
+
